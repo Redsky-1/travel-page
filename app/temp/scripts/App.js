1460f8c80713385d68ef882d97dd727c47377dfe
Richard <rdavies_7@yahoo.co.uk>
2018-11-03 15:33:09 +0000

    Completed lazy loading of images

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11238,6 +11238,7 @@
 		function StickyHeader() {
 			_classCallCheck(this, StickyHeader);
 
+			this.lazyImages = (0, _jquery2.default)(".lazyload");
 			this.siteHeader = (0, _jquery2.default)(".site-header");
 			this.headerTriggerElement = (0, _jquery2.default)(".large-hero__title");
 			this.createHeaderWaypoint();
@@ -11245,9 +11246,17 @@
 			this.headerLinks = (0, _jquery2.default)(".primary-nav a");
 			this.createPageSectionWaypoints();
 			this.addSmothScrolling();
+			this.refreshWaypoints();
 		}
 
 		_createClass(StickyHeader, [{
+			key: 'refreshWaypoints',
+			value: function refreshWaypoints() {
+				this.lazyImages.on('load', function () {
+					Waypoint.refreshAll();
+				});
+			}
+		}, {
 			key: 'addSmothScrolling',
 			value: function addSmothScrolling() {
 				this.headerLinks.smoothScroll();
